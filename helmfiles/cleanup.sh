3696ffe8c4c1fb150626715066218c0abd3146b5
Mattia Bradascio <28816406+bredamatt@users.noreply.github.com>
2023-01-24 14:19:32 +0000

    Add simple cleanup script

---
+++ helmfiles/cleanup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+kubectl delete pvc data-cassandra-0 data-cassandra-1 data-cassandra-2 -n observability
+kubectl delete pvc chain-data-alice-2k-node-0 chain-data-validator-alice-node-0 chain-data-validator-bob-node-0 relaychain-data-alice-2k-node-0 -n rococo
+
