9fd476f39131fa190983e551450ca23097f245f4
Mattia Bradascio <28816406+bredamatt@users.noreply.github.com>
2023-01-30 17:55:03 +0000

    Remove submodule. Delete all namespaces in cleanup script.

--- helmfiles/cleanup.sh
+++ helmfiles/cleanup.sh
@@ -7,3 +7,4 @@ kubectl delete pvc data-cassandra-0 data-cassandra-1 data-cassandra-2 -n observa
 kubectl delete namespace observability
 kubectl delete pvc chain-data-alice-2k-node-0 chain-data-validator-alice-node-0 chain-data-validator-bob-node-0 relaychain-data-alice-2k-node-0 -n rococo
 kubectl delete namespace rococo
+kubectl delete namespace observability
