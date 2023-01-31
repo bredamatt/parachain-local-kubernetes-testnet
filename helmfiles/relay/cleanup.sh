99154882add0681788c55a45611a38d6c2f014f7
Mattia Bradascio <28816406+bredamatt@users.noreply.github.com>
2023-01-31 16:16:53 +0000

    Add namespace

--- helmfiles/relay/cleanup.sh
+++ helmfiles/relay/cleanup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 helmfile delete
-kubectl delete pvc chain-data-validator-alice-node-0 chain-data-validator-bob-node-0
+kubectl delete pvc chain-data-validator-alice-node-0 chain-data-validator-bob-node-0 -n rococo
